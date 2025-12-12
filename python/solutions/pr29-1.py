945b8a2f7667d6b3e72edb14d79c81ba6813ff0e
shaia <shaia@outlook.com>
2025-12-12 14:38:08 +0200

    Removing old code structure

--- python/solutions/pr29-1.py
+++
@@ -1,1 +0,0 @@
-print(len({base ** power for base in range(2,101) for power in range(2,101)}))
--- python/solutions/pr29-3.py
+++
@@ -1,3 +0,0 @@
-import itertools
-
-print (len(set(a ** b for (a, b) in itertools.combinations(range(2, 101) * 2, 2))))
